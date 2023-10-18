0f8844c502f86f8434123b90f7a40b6f19f48158
dialgacylert <dcylert@gmail.com>
2023-10-18 21:25:16 +0700

    Initial Commit

---
+++ app/src/main/java/com/arfsar/storyapp/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.arfsar.storyapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arfsar.storyapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/arfsar/storyapp/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.arfsar.storyapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arfsar.storyapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
