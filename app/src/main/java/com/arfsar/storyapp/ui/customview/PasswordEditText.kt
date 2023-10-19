235126313994592d476ed4c8721482263ab09fa0
dialgacylert <dcylert@gmail.com>
2023-10-19 15:16:05 +0700

    Create layout login and signup, and create editText customView

---
+++ app/src/main/java/com/arfsar/storyapp/ui/customview/PasswordEditText.kt
@@ -0,0 +1,65 @@
+package com.arfsar.storyapp.ui.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class PasswordEditText : AppCompatEditText {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        hint = "Input Your Password"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+    private fun init() {
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().length < 8) {
+                    setError("Password cannot be less than 8 characters", null)
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+    }
+
+//    private fun setButtonDrawables(
+//        startOfTheText: Drawable? = null,
+//        topOfTheText: Drawable? = null,
+//        endOfTheText: Drawable? = null,
+//        bottomOfTheText: Drawable? = null
+//    ) {
+//        setCompoundDrawablesWithIntrinsicBounds(
+//            startOfTheText,
+//            topOfTheText,
+//            endOfTheText,
+//            bottomOfTheText
+//        )
+//    }
+
+}
